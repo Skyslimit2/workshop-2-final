de00c8061ec3d341f668995c232fa773e41e1236
Skyslimit2 <snakesupplier@gmail.com>
2025-10-28 17:13:36 -0400

    dsd

--- sketch.js
+++ sketch.js
@@ -30,19 +30,19 @@ function setup() {
 function draw() {
   background(0, 0, 240);
 
-  let scaleX = width / walkL.width;
-  let scaleY = height / walkL.height;
-  let scale = max(scaleX, scaleY);
-
-
-  let scaledWidth = walkL.width * scale;
-  let scaledHeight = walkL.height * scale;
-  let x = (width - scaledWidth) / 2;
-  let y = (height - scaledHeight) / 2;
-
-  image(walkL, x, y, scaledWidth, scaledHeight);
-
   if (window.sensorsEnabled){
     let tilt = rotationY
   }
+
+  let scaleX = width / walkS.width;
+  let scaleY = height / walkS.height;
+  let scale = max(scaleX, scaleY);
+
+
+  let scaledWidth = walkS.width * scale;
+  let scaledHeight = walkS.height * scale;
+  let x = (width - scaledWidth) / 2;
+  let y = (height - scaledHeight) / 2;
+
+  image(walkS, x, y, scaledWidth, scaledHeight);
 }
